f42ec0fad60bd37ef63e79f28603d2fbd028dc4c
Ayat Tulloh Rahulloh Khomeini <5026211156@mhs.its.ac.id>
2023-06-27 01:53:00 +0700

    Module 2

---
+++ Module 2 - Python Data Structures/Lists and Tuples.py
@@ -0,0 +1,19 @@
+#Tuples
+tuple1=(10, "sesa", 3.6)
+print(tuple1[1])
+tuple2=tuple1 + ("cantik", 100.9, 99)
+print(tuple2)
+#Tuple: slicing
+print(tuple2[0:4])
+print(len(tuple2))
+#Tuple: immutable
+tuple3=(11, 993.019,-113,-139821.2232)
+print(sorted(tuple3))
+#Tuple: nesting
+tuple4=(1,2,("Shafaa","Dimitri"),3.2)
+print(len(tuple4))
+print(tuple4[2])
+print(tuple4[2][1])
+
+#Lists
+List = ["Sesa",10, 9.2]
