3a792af1c82165d76b395154e4a9a79b8bf82020
Ayat Tulloh Rahulloh Khomeini <5026211156@mhs.its.ac.id>
2023-06-27 20:19:22 +0700

    Module 2: Sets

--- Module 2 - Python Data Structures/Lists and Tuples.py
+++ Module 2 - Python Data Structures/Lists and Tuples.py
@@ -17,3 +17,31 @@ print(tuple4[2][1])
 
 #Lists
 List = ["Sesa",10, 9.2]
+print(List[0:2])
+Lists = List + ["Shadiim"]
+print(Lists)
+
+List.extend(Lists)
+print(List)
+
+List.append(Lists)
+print(List)
+
+del(List[1])
+print(List)
+
+#Convert string to list
+masaDepan="Shafaa Dimitri Azalea".split()
+print(masaDepan)
+namaMama ="sesa, shafa, shadim".split(",")
+print(namaMama)
+
+#List: aliasing
+A=["Azalea"]
+B=["Sesaa"]
+B = A
+print(B[0])
+
+#List: clone
+B=A[:]
+print(B)
