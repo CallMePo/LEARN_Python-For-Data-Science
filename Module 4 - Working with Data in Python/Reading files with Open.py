150d41c9dd647fdd0f3b9cf0d310f4860562c68b
Ayat Tulloh Rahulloh Khomeini <5026211156@mhs.its.ac.id>
2023-07-13 03:26:43 +0700

    Module 4 - Working with Data in Python Reading files with Open

--- Module 4 - Working with Data in Python/Reading files with Open.py
+++ Module 4 - Working with Data in Python/Reading files with Open.py
@@ -1,8 +1,8 @@
 with open("example.txt",'w') as file:# 'w' for write, 'r' for read
-    file_stuff=file.write("Shafaa Dimitri Azalea \nShadim")
+    file_stuff=file.write("Shafaa Dimitri Azalea\nShadim")
 with open("example.txt", 'r')as file2:
     file_stuff2=file2.read()
     print(file_stuff2)
 with open("example.txt", 'r') as file:
-    fileku=file.readlines(1)
+    fileku=file.readlines()
     print(fileku)
